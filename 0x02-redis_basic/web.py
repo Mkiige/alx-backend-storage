4f9eed7f8e2202498f8ce92f681a53cd3bbf5d41
Mkiige <mkiige77@gmail.com>
2023-05-14 20:37:49 +0300

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""Module tracks how many times a particular URL was accessed"""
 import redis
 import requests
-from datetime import timedelta
+from typing import Callable
+from functools import wraps
 
 
+def count(method: Callable):
+    '''track how many times a particular URL was accessed'''
+    r = redis.Redis()
+
+    @wraps(method)
+    def wrapper(url):
+        '''wrap the decorated function and return the wrapper'''
+        r.incr(f"count:{url}")
+        exp_count = r.get(f"cached:{url}")
+        if exp_count:
+            return exp_count.decode('utf-8')
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+    return wrapper
+
+
+@count
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    if url is None or len(url.strip()) == 0:
-        return ''
-    redis_store = redis.Redis()
-    res_key = 'result:{}'.format(url)
-    req_key = 'count:{}'.format(url)
-    result = redis_store.get(res_key)
-    if result is not None:
-        redis_store.incr(req_key)
-        return result
-    result = requests.get(url).content.decode('utf-8')
-    redis_store.setex(res_key, timedelta(seconds=10), result)
-    return result
+    '''uses the requests module to obtain the HTML content'''
+    return requests.get(url).text
