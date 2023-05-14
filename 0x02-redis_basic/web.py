41af90859bec5ae74580d416f0183baab71f10c7
Mkiige <mkiige77@gmail.com>
2023-05-14 20:34:56 +0300

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,10 @@
-#!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
 import requests
-from datetime import timedelta
+from cachetools import cached, TTLCache
 
+cache = TTLCache(maxsize=100, ttl=10)  # Cache with maxsize of 100 and expiration time of 10 seconds
 
+@cached(cache)
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    if url is None or len(url.strip()) == 0:
-        return ''
-    redis_store = redis.Redis()
-    res_key = 'result:{}'.format(url)
-    req_key = 'count:{}'.format(url)
-    result = redis_store.get(res_key)
-    if result is not None:
-        redis_store.incr(req_key)
-        return result
-    result = requests.get(url).content.decode('utf-8')
-    redis_store.setex(res_key, timedelta(seconds=10), result)
-    return result
+    """Retrieve HTML content of a webpage and cache the result for 10 seconds."""
+    response = requests.get(url)
+    return response.content.decode('utf-8')
